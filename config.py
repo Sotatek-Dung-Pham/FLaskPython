07fadf7a453d7edee80848e8a4ead3418a6ab170
Dung <dung.pham@sotatek.com>
2023-03-02 14:37:41 +0700

    test

--- config.py
+++ config.py
@@ -32,6 +32,13 @@ class ProductionConfig(Config):
     Envirement production
     """
 
+    JWT_SECRET_KEY = 'super-secret' # Change this!
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours = 1)
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(days = 30)
+    SQLALCHEMY_DATABASE_URI = 'postgresql://dungAws:@52.68.119.104/myAwsdata'
+    TEMPLATES_AUTO_RELOAD = True
+
     DEBUG = False
 
 app_config = {
