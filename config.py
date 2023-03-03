0dc7d35eecb38ddbf29f25e03966ce7da49f141f
Dung <dung.pham@sotatek.com>
2023-03-03 11:48:29 +0700

    test

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ class ProductionConfig(Config):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours = 1)
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(days = 30)
-    SQLALCHEMY_DATABASE_URI = 'postgresql://dungAws:@52.68.119.104:5433/myawsdata'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://dungAws:@54.178.69.80:5433/myawsdata'
     TEMPLATES_AUTO_RELOAD = True
 
     DEBUG = False
